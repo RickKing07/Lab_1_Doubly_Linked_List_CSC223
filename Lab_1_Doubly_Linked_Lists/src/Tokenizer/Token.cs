1d0d656c8a999c7498a8eb9bc8e52f63c9c4abef
Rick K <rick.the.southpaw@gmail.com>
2025-09-29 14:22:18 -0400

    pre-lab progress, token structure part implimented

--- Lab_1_Doubly_Linked_Lists/src/Tokenizer/Token.cs
+++ Lab_1_Doubly_Linked_Lists/src/Tokenizer/Token.cs
@@ -6,18 +6,65 @@ namespace Tokenizer
 
     public class TokenType()
     {
-        //emumeration
+        public enum PrimitiveTypes
+        {
+            //Keywords & Variables, should it just be VAR & Return?
+            RETURN,
+            IF,
+            ELSE,
+            WHILE,
+            FOR,
+            VAR,
+            STRING,
+            BOOL,
+            VOID
+        }
+
+        public enum Literals
+        {
+            //Integer & floats
+
+            INT,
+            FLOAT
+        }
+
+        public enum Operators
+        // Assigment, + - * / // %,  to OPERATOR or ASSIGMENT?
+        {
+            ASSIGMENT,
+            PLUS,
+            MINUS,
+            TIMES,
+            FLOAT_DIVISION,
+            INT_DIVISION,
+            MOD,
+            EXPONENT
+        }
+
+        public enum StructureAndGrouping
+        //Parentheses & curly braces to LEFT_PAREN, RIGHT_PAREN, LEFT_CURLY, RIGHT_CURLY
+        {
+            LEFT_PAEREN,
+            RIGHT_PAREN,
+            LEFT_CURLY,
+            RIGHT_CURLY
+        }
     }
-    public static class TokenConstants()
+    public static class TokenConstants
     {
         //Plus, LEFT_PAEREN, LEFT_CURLY, ASSIGMENT, DECIMAL_POINT contstatns defined
+
+        TokenType.Operators PLUS = "+"; //check if this is correct
     }
-    public class Token()
+    public class Token(char x) //better name -_-
     {
         //aggregating structure that gets tokentype and value of a token
-        //2 properties, value and type
-        //standard class constructors liek to string, equals, and more
+
+        public TokenType Type { get; set; }
+        public char Value { get; set; } //might could be readonly
+        //standard class constructors like to string, equals, and more
     }
 }
+
 
 
