be049f3ad7e30306cf0b83aaae5059f4f496f710
Rick K <rick.the.southpaw@gmail.com>
2025-09-29 16:19:28 -0400

    Skeleton and intitial progress on impl

--- Lab_1_Doubly_Linked_Lists/src/Tokenizer/TokenizerImpl.cs
+++ Lab_1_Doubly_Linked_Lists/src/Tokenizer/TokenizerImpl.cs
@@ -1,7 +1,49 @@
+//using TokenConstants?
 public class TokenizerImpl
 {
-    public List<Token> Tokenize(string code)
+    public List<Token> Tokenize(string input)
     {
+        List<Token> TokenList = [];
+        int index = 0;
+        while (index < input.Length())
+        {
+            if (char.IsDigit(input[index]))
+            {
+                TokenList.Add(PrimitiveH(input[index]));
+            }
 
+        }
+        //Determine Token type
+        //call appropriate helper method that returns a token
+        //add token to big list
+        //return bit list<Token>
+    }
+
+    public Token PrimitiveH(char letter)
+    {
+        //determine type (VARIABLE or RETURN)
+        //Feed it to token init with char && type
+        //return Token
+    }
+
+    public Token LiteralH(char letter)
+    {
+        //determine type (INTEGER or FLOAT)
+        //Feed it to token init with char && type
+        //return Token
+    }
+
+    public Token OperatorsH(char letter)
+    {
+        //determine type (Operator or ASSIGMENT)
+        //Feed it to token init with char && type
+        //return Token
+    }
+
+    public Token StructureH(char letter)
+    {
+        //determine type (LEFT_PAREN or RIGHT_CURLY ect.)
+        //Feed it to token init with char && type
+        //return Token
     }
 }
