55611c8550a15ec7d19d1c77d0d5478a23a207ce
Rick K <rick.the.southpaw@gmail.com>
2025-09-29 13:43:02 -0400

    Skeleton

---
+++ Lab_1_Doubly_Linked_Lists/src/Tokenizer/Token.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace Tokenizer
+{
+    //enum and class def
+
+    public class TokenType()
+    {
+        //emumeration
+    }
+    public static class TokenConstants()
+    {
+        //Plus, LEFT_PAEREN, LEFT_CURLY, ASSIGMENT, DECIMAL_POINT contstatns defined
+    }
+    public class Token()
+    {
+        //aggregating structure that gets tokentype and value of a token
+        //2 properties, value and type
+        //standard class constructors liek to string, equals, and more
+    }
+}
+
+
