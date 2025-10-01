c3b1f34b713e90a25c72524300ff796426bf1184
Rick K <rick.the.southpaw@gmail.com>
2025-10-01 14:42:57 -0400

    start of fixing things

--- Lab_1_Doubly_Linked_Lists/src/Tokenizer/TokenizerImpl.cs
+++ Lab_1_Doubly_Linked_Lists/src/Tokenizer/TokenizerImpl.cs
@@ -3,60 +3,66 @@ public class TokenizerImpl
 {
     public List<Token> Tokenize(string input)
     {
-        //split input, loop through split results
-        string[] inputList = input.Split(" ");
         //!! for skipping indicies, use index ref/out var to manipulate it from within methods
         List<Token> TokenList = [];
         int index = 0;
-        while (index < inputList.Length)
+        while (index < input.Length)
         {
-            string currentSegment = inputList[index];
-            char currentChar = currentSegment[0];
-            if (char.IsDigit(currentChar))
-            {
-                TokenList.Add(LiteralH(currentSegment));
-            }
-            if (char.IsLetter(currentChar))
-            {
-                TokenList.Add(PrimitiveH(currentSegment));
-            }
-            if (!char.IsWhiteSpace(currentChar))
-            {
-                if (currentSegment == "{" || currentSegment == "}" || currentSegment == "(" || currentSegment == ")")
-                {
-                    TokenList.Add(StructureH(currentSegment));
-                }
-
-                else
-                {
-                    TokenList.Add(OperatorsH(currentSegment));
-                }
-            }
+            char currentChar = input[index];
+            if (char.IsWhiteSpace(currentChar)) { continue; }
+            if (char.IsDigit(currentChar)) { TokenList.Add(LiteralH(input, index)); }
+            if (char.IsLetter(currentChar)) { TokenList.Add(PrimitiveH(input, index)); }
+            if (currentChar == "{" || currentChar == "}" || currentChar == "(" || currentChar == ")") { TokenList.Add(StructureH(currentChar)); }
+            else { TokenList.Add(OperatorsH(input, index)); }
             index++;
         }
-        return TokenList;
 
+        return TokenList;
     }
+
+
     //Determine Token type
     //call appropriate helper method that returns a token
     //add token to big list
     //return bit list<Token>
 
 
-    public Token PrimitiveH(string item)
+    public Token PrimitiveH(string input, ref int index)
     {
+        string item = "";
+        while (char.IsLetter(input[index]))
+        {
+            item = item + input[index];
+            index++;
+        }
         if (item == "return") { return new Token(item, TokenType.RETURN); }
-        else { return new Token(item, TokenType.VARIABLE); }
-
+        else return new Token(item, TokenType.VARIABLE);
     }
 
-    public Token LiteralH(string item)
+
+
+    public Token LiteralH(string input, ref int index)
     {
-        if (item != item.Split(".")[0]) { return new Token(item, TokenType.FLOAT); }
-        else { return new Token(item, TokenType.FLOAT); }
+        string item = "";
+        while (char.IsDigit(input[index]))
+        {
+            item = item + input[index];
+            index++;
+        }
+        if (input[index] == ".")
+        {
+            while (char.IsDigit(input[index]))
+            {
+                item = item + input[index];
+                index++;
+            }
+            return new Token(item, TokenType.FLOAT);
+        }
+        else return new Token(item, TokenType.INTEGER);
+
     }
 
-    public Token OperatorsH(string item)
+    public Token OperatorsH(string input, ref int index)
     {
         //determine type (Operator or ASSIGMENT)
         //Feed it to token init with char && type
@@ -96,7 +102,7 @@ public class TokenizerImpl
 
     }
 
-    public Token StructureH(string item)
+    public Token StructureH(char letter)
     {
         if (item == "{") { return new Token(item, TokenType.LEFT_CURLY); }
         else if (item == "}") { return new Token(item, TokenType.RIGHT_CURLY); }
